5cffe8a5b089434eedacef007235373558d11909
Dieudonne Ntakirutimana <98171713+ledidk@users.noreply.github.com>
2024-04-21 20:30:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,12 +16,18 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
-                            <Budget />
+                            /* Add Budget component here */ 
+                            <div className='col-sm'>
+                                  <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
+                            
                         }        
 
                         {
